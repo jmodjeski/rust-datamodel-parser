90dc8b6178b8fc4b72400d881c84cfe71aa09b46
Joe Modjeski <joe@modjeski.net>
2018-10-24 22:53:11 -0700

    just playing around

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use std::env;
+use std::fs::File;
+use std::io::{Read};
+use std::path::Path;
+
+pub struct DataModelType<'a> {
+    name: &'a str
+}
+
+mod my_grammar {
+    include!(concat!(env!("OUT_DIR"), "/my_grammar.rs"));
+}
+
+fn main() {
+	let args = env::args_os().collect::<Vec<_>>();
+	let mut source = String::new();
+    File::open(Path::new(&args[1])).unwrap().read_to_string(&mut source).unwrap();
+
+    match my_grammar::modelType(&source) {
+        Ok(r) => println!("Parsed as: {}", r.name),
+        Err(e) => println!("Parse error: {}", e),
+    }
+}
