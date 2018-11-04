b9fc19a00c93994d65baa9f7c26b8a360f5665b0
Joe Modjeski <joe@modjeski.net>
2018-11-04 15:09:29 -0700

    feat(output): file or stdout

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,10 @@ extern crate serde_derive;
 extern crate serde_json;
 
 use std::fs::File;
+use std::fs::OpenOptions;
 use std::io::stdin;
-use std::io::Read;
+use std::io::stdout;
+use std::io::{Read, Write};
 use std::path::Path;
 use std::process::exit;
 
@@ -35,7 +37,27 @@ fn main() {
     }
 
     match datamodel_parser::models(&source) {
-        Ok(r) => println!("{}", serde_json::to_string_pretty(&r).unwrap()),
+        Ok(r) => {
+            match serde_json::to_string_pretty(&r) {
+                Ok(buf) => {
+                    if matches.is_present("stdout") {
+                        let x = write_stdout(buf);
+                        if x.is_err() {
+                            eprintln!("Error writing to STDOUT {}", x.err().unwrap());
+                            exit(1);
+                        }
+                    } else {
+                        let v = matches.value_of("output").unwrap();
+                        let x = write_output(&v, buf);
+                        if x.is_err() {
+                            eprintln!("error writing to {}: {}", v, x.err().unwrap());
+                            exit(1);
+                        }
+                    }
+                },
+                Err(e) => eprintln!("Format error: {}", e),
+            }
+        },
         Err(e) => eprintln!("Parse error: {}", e),
     }
 }
@@ -49,5 +71,18 @@ fn read_datamodel(path: &str, source: &mut String) -> std::io::Result<()> {
     let resolved_path = Path::new(&path);
     let mut file = File::open(resolved_path)?;
     file.read_to_string(&mut *source)?;
+    Ok(())
+}
+
+fn write_stdout(source: String) -> std::io::Result<()> {
+    // stdin().read_to_string(&mut *source)?;
+    stdout().write(&source.into_bytes())?;
+    Ok(())
+}
+
+fn write_output(path: &str, source: String) -> std::io::Result<()> {
+    let resolved_path = Path::new(&path);
+    let mut file = OpenOptions::new().create(true).write(true).open(resolved_path)?;
+    file.write(&source.into_bytes())?;
     Ok(())
 }
