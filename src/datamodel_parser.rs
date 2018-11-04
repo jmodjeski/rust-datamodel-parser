be75e5d49b0917a9442d057821fd00418bdcb5c0
Joe Modjeski <joe@modjeski.net>
2018-11-04 08:17:07 -0700

    chore(style): rust-fmt and forget

--- src/datamodel_parser.rs
+++ src/datamodel_parser.rs
@@ -1,29 +1,30 @@
-  #[derive(Serialize)]
-  pub struct DataModelTypeDeclaration<'a> {
-      pub name: &'a str,
-      pub fields: Vec<DataModelFieldDeclaration<'a>>,
-  }
 
-  #[derive(Serialize)]
-  pub struct DataModelFieldDeclaration<'a> {
-      pub name: &'a str,
-      pub field_type: &'a str,
-      pub required: bool,
-      pub directives: Vec<DataModelFieldDirective<'a>>
-  }
+#[derive(Serialize)]
+pub struct DataModelTypeDeclaration<'a> {
+    pub name: &'a str,
+    pub fields: Vec<DataModelFieldDeclaration<'a>>,
+}
 
-  #[derive(Serialize)]
-  pub struct DataModelFieldDirective<'a> {
-      pub name: &'a str,
-      pub arguments: Vec<DataModelFieldDirectiveArg<'a>>,
-  }
+#[derive(Serialize)]
+pub struct DataModelFieldDeclaration<'a> {
+    pub name: &'a str,
+    pub field_type: &'a str,
+    pub required: bool,
+    pub directives: Vec<DataModelFieldDirective<'a>>,
+}
 
-  #[derive(Serialize)]
-  pub struct DataModelFieldDirectiveArg<'a> {
-      pub name: &'a str,
-      pub value: String,
-      pub quoted: bool,
-  }
+#[derive(Serialize)]
+pub struct DataModelFieldDirective<'a> {
+    pub name: &'a str,
+    pub arguments: Vec<DataModelFieldDirectiveArg<'a>>,
+}
 
-  include!(concat!(env!("OUT_DIR"), "/datamodel_grammar.rs"));
-  
+#[derive(Serialize)]
+pub struct DataModelFieldDirectiveArg<'a> {
+    pub name: &'a str,
+    pub value: String,
+    pub quoted: bool,
+}
+
+#[cfg_attr(feature = "cargo-clippy", allow(clippy_all))]
+include!(concat!(env!("OUT_DIR"), "/datamodel_grammar.rs"));
--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,25 @@ extern crate serde_derive;
 extern crate serde_json;
 
 use std::fs::File;
-use std::io::{Read};
+use std::io::Read;
 use std::path::Path;
 
-use clap::{App};
+use clap::App;
 
 mod datamodel_parser;
 
 fn main() {
-  let yaml = load_yaml!("cli.yml");
-  let matches = App::from_yaml(yaml).get_matches();
-  let datamodel_file = matches.value_of("data_model").unwrap();
-  let mut source = String::new();
-  File::open(Path::new(&datamodel_file)).unwrap().read_to_string(&mut source).unwrap();
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+    let datamodel_file = matches.value_of("data_model").unwrap();
+    let mut source = String::new();
+    File::open(Path::new(&datamodel_file))
+        .unwrap()
+        .read_to_string(&mut source)
+        .unwrap();
 
-  match datamodel_parser::models(&source) {
-    Ok(r) => println!("Parsed as: {}", serde_json::to_string_pretty(&r).unwrap()),
-    Err(e) => println!("Parse error: {}", e),
-  }
+    match datamodel_parser::models(&source) {
+        Ok(r) => println!("Parsed as: {}", serde_json::to_string_pretty(&r).unwrap()),
+        Err(e) => println!("Parse error: {}", e),
+    }
 }
