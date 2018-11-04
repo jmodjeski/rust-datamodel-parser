211a4c4148edc49d81edae76781fc0cc8db2ce37
Joe Modjeski <joe@modjeski.net>
2018-11-03 21:17:01 -0700

    some more moving stuff around.

---
+++ src/datamodel_parser.rs
@@ -0,0 +1,29 @@
+  #[derive(Serialize)]
+  pub struct DataModelTypeDeclaration<'a> {
+      pub name: &'a str,
+      pub fields: Vec<DataModelFieldDeclaration<'a>>,
+  }
+
+  #[derive(Serialize)]
+  pub struct DataModelFieldDeclaration<'a> {
+      pub name: &'a str,
+      pub field_type: &'a str,
+      pub required: bool,
+      pub directives: Vec<DataModelFieldDirective<'a>>
+  }
+
+  #[derive(Serialize)]
+  pub struct DataModelFieldDirective<'a> {
+      pub name: &'a str,
+      pub arguments: Vec<DataModelFieldDirectiveArg<'a>>,
+  }
+
+  #[derive(Serialize)]
+  pub struct DataModelFieldDirectiveArg<'a> {
+      pub name: &'a str,
+      pub value: String,
+      pub quoted: bool,
+  }
+
+  include!(concat!(env!("OUT_DIR"), "/datamodel_grammar.rs"));
+  
--- src/main.rs
+++ src/main.rs
@@ -7,24 +7,14 @@ use std::fs::File;
 use std::io::{Read};
 use std::path::Path;
 
-mod ast_types {
-    include!("./ast_types.rs");
-}
-
-mod datamodel_grammar {
-    use ast_types::DataModelTypeDeclaration;
-    use ast_types::DataModelFieldDeclaration;
-    use ast_types::DataModelFieldDirective;
-    use ast_types::DataModelFieldDirectiveArg;
-    include!(concat!(env!("OUT_DIR"), "/datamodel_grammar.rs"));
-}
+mod datamodel_parser;
 
 fn main() {
 	let args = env::args_os().collect::<Vec<_>>();
 	let mut source = String::new();
     File::open(Path::new(&args[1])).unwrap().read_to_string(&mut source).unwrap();
 
-    match datamodel_grammar::models(&source) {
+    match datamodel_parser::models(&source) {
         Ok(r) => println!("Parsed as: {}", serde_json::to_string_pretty(&r).unwrap()),
         Err(e) => println!("Parse error: {}", e),
     }
