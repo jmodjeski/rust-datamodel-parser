2b2e0b4df5abd09601488845e767b8dfe0c158dd
Joe Modjeski <joe@modjeski.net>
2018-11-03 21:45:16 -0700

    playing with args, :clap:

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,26 @@
 #[macro_use]
+extern crate clap;
+#[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
 
-use std::env;
 use std::fs::File;
 use std::io::{Read};
 use std::path::Path;
 
+use clap::{App};
+
 mod datamodel_parser;
 
 fn main() {
-	let args = env::args_os().collect::<Vec<_>>();
-	let mut source = String::new();
-    File::open(Path::new(&args[1])).unwrap().read_to_string(&mut source).unwrap();
+  let yaml = load_yaml!("cli.yml");
+  let matches = App::from_yaml(yaml).get_matches();
+  let datamodel_file = matches.value_of("data_model").unwrap();
+  let mut source = String::new();
+  File::open(Path::new(&datamodel_file)).unwrap().read_to_string(&mut source).unwrap();
 
-    match datamodel_parser::models(&source) {
-        Ok(r) => println!("Parsed as: {}", serde_json::to_string_pretty(&r).unwrap()),
-        Err(e) => println!("Parse error: {}", e),
-    }
+  match datamodel_parser::models(&source) {
+    Ok(r) => println!("Parsed as: {}", serde_json::to_string_pretty(&r).unwrap()),
+    Err(e) => println!("Parse error: {}", e),
+  }
 }
