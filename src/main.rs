7d75cfa4330754c4822bf5fb9255b0c3c233edeb
Joe Modjeski <joe@modjeski.net>
2018-11-04 10:09:13 -0700

    feat(input): read stdin or --data-model

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ extern crate serde_json;
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
+use std::io::stdin;
+use std::process::exit;
 
 use clap::App;
 
@@ -15,12 +17,42 @@ mod datamodel_parser;
 fn main() {
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
-    let datamodel_file = matches.value_of("data_model").unwrap();
     let mut source = String::new();
-    File::open(Path::new(&datamodel_file))
-        .unwrap()
-        .read_to_string(&mut source)
-        .unwrap();
+
+    if matches.is_present("stdin") {
+      match stdin().read_to_string(&mut source) {
+        Ok(r) => println!("Read {} bytes from STDIN", r),
+        Err(e) => {
+          println!("Error reading from STDIN {}", e);
+          exit(1);
+        }
+      };
+    } else {
+      match matches.value_of("data_model") {
+        Some(v) => {
+          match File::open(Path::new(&v)) {
+            Err(e) => {
+              println!("Invalid data-model file: {}", e);
+              exit(1);
+            },
+            Ok(mut f) => 
+              match f.read_to_string(&mut source) {
+                Err(e) => {
+                  println!("Invalid data-model file {}", e);
+                  exit(1);
+                },
+                Ok(v) => {
+                  println!("read {} bytes", v, );
+                }
+              }
+          }
+        },
+        None => {
+          println!("arguments error --stdin or --data-model must be present");
+          exit(1)
+        }
+      };
+    }
 
     match datamodel_parser::models(&source) {
         Ok(r) => println!("Parsed as: {}", serde_json::to_string_pretty(&r).unwrap()),
