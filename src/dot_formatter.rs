2f57b4b96152be9d8f14539407ec9525c219dc32
Joe Modjeski <joe@modjeski.net>
2018-11-06 07:50:49 -0700

    add in simple linking

--- src/dot_formatter.rs
+++ src/dot_formatter.rs
@@ -1,15 +1,23 @@
 use datamodel_parser::RootTypes;
 use datamodel_parser::DataModelTypeRef;
+use datamodel_parser::DataModelFieldDeclaration;
 use std::string::String;
 use std::fmt::{Write, Error};
+use std::collections::HashMap;
 
 pub struct DOTFormatterOptions {}
+
+struct Port {
+    left: Option<String>,
+    right: Option<String>,
+}
 
 pub fn format(
     _options: DOTFormatterOptions,
     types: Vec<RootTypes>,
 ) -> Result<String, Error> {
     let mut value = String::new();
+    let mut ports: HashMap<String, Port> = HashMap::new();
 
     value.write_str("digraph model {\r\n")?;
 
@@ -28,14 +36,48 @@ pub fn format(
 
                 let field_iter = t.fields.iter();
                 for field in field_iter {
-                    let t = get_field_type(&field.field_type);
-                    value.write_str(&format!("\t\t\t\t<tr><td>{}</td><td>{}</td></tr>\r\n", field.name, t))?;
+                    let field_type = get_field_type(&field.field_type);
+                    let relation = get_relation_directive(&field);
+                    match relation {
+                        None => value.write_str(&format!("\t\t\t\t<tr><td>{}</td><td>{}</td></tr>\r\n", field.name, field_type))?,
+                        Some(relation_name) => {
+                            let port_name = relation_name.clone();
+                            let entry_name = relation_name.clone();
+                            let root_type = get_root_field_type(&field.field_type);
+                            let mut port = ports.entry(entry_name).or_insert(Port{
+                                left: None,
+                                right: None
+                            });
+                            if field_type.starts_with("[") {
+                                port.right = Some(format!("table{}", root_type));
+                            } else {
+                                port.left = Some(format!("table{}", root_type));
+                            }
+                            value.write_str(&format!("\t\t\t\t<tr><td port='{}'>{}</td><td>{}</td></tr>\r\n", port_name, field.name, field_type))?;
+                        }
+                    }
+                    
                 }
 
                 value.write_str("\t\t\t</table>")?;
-                value.write_str(">\t]\r\n")?;
+                value.write_str(">\t];\r\n")?;
             }
         }
+    }
+
+    let port_iter = ports.iter();
+    for port_entry in port_iter {
+        let port = &port_entry.1;
+        let port_name = port_entry.0;
+        let left_side = match port.left {
+            Some(ref v) => v.clone(),
+            None => "<missing>".to_string()
+        };
+        let right_side = match port.right {
+            Some(ref v) => v.clone(),
+            None => "<missing>".to_string()
+        };
+        value.write_str(&format!("\t{}:{} -> {}:{};\r\n", left_side, port_name, right_side, port_name))?;
     }
 
     value.write_str("}\r\n")?;
@@ -51,4 +93,27 @@ fn get_field_type(field: &DataModelTypeRef) -> String {
         Some(v) => format!("[{}]{}", get_field_type(v.as_ref()), required),
         None => format!("{}{}", field.name, required),
     }
+}
+
+fn get_root_field_type(field: &DataModelTypeRef) -> String {
+    let ref reffed = field.inner_type;
+    match reffed {
+        Some(v) => get_root_field_type(v.as_ref()),
+        None => field.name.to_owned(),
+    }
+}
+
+fn get_relation_directive(field: &DataModelFieldDeclaration) -> Option<String> {
+    let directive_iter = field.directives.iter();
+    for directive in directive_iter {
+        if directive.name == "relation" {
+            let arg_iter = directive.arguments.iter();
+            for arg in arg_iter {
+                if arg.name == "name" {
+                    return Some(arg.value.to_owned());
+                }
+            }
+        }
+    };
+    None
 }
